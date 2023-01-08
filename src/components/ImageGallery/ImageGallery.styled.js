d096c2f84b6c2d5c80b7aa31c05a834e4db2b3cf
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-08 19:11:15 +0100

    Update ImageGallery.styled.js

--- src/components/ImageGallery/ImageGallery.styled.js
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const GalleryList = styled.ul`
   display: grid;
-  max-width: calc(100vw - 48px);
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  grid-gap: 16px;
+  max-width: calc(200vw - 40px);
+  grid-template-columns: repeat(auto-fill, minmax(260px, 460px));
+  grid-gap: 20px;
   margin-top: 0;
   margin-bottom: 0;
   padding: 0;
