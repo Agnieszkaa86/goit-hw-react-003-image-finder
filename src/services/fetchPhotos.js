35a3c99231b31e000bf71c77303e26edb4c57d2e
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-08 19:11:24 +0100

    Update fetchPhotos.js

--- src/services/fetchPhotos.js
+++ src/services/fetchPhotos.js
@@ -18,24 +18,3 @@ export const fetchPhotos = async (searchP, currentPage) => {
   return responseData;
 };
 
-// import axios from 'axios';
-
-// export const fetchPhotos = async (name, page) => {
-//   const searchParams = new URLSearchParams({
-//     key: '28056380-7ebf030984661b6034d156d96',
-//     q: name,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: 'true',
-//     per_page: 12,
-//     page,
-//   });
-//   try {
-//     const response = await axios.get(
-//       `https://pixabay.com/api/?${searchParams}`
-//     );
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
