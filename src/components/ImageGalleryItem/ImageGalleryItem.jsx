134cddd54cdb0391d554bfdb49436e8f32c3ad26
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-08 19:11:17 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import { GalleryItem, Image } from './ImageGalleryItem.styled';
 
-export const ImageGalleryItem = ({ webformatURL, largeImageURL }) => {
+export const ImageGalleryItem = ({ webformatURL, largeImageURL,alt}) => {
   return (
     <GalleryItem>
-      <Image src={webformatURL} alt="" data-source={largeImageURL} />
+      <Image src={webformatURL} alt={alt} data-source={largeImageURL} />
     </GalleryItem>
   );
 };
@@ -12,4 +12,5 @@ export const ImageGalleryItem = ({ webformatURL, largeImageURL }) => {
 ImageGalleryItem.propTypes = {
   webformatURL: PropTypes.string.isRequired,
   largeImageURL: PropTypes.string.isRequired,
+  alt: PropTypes.string,
 };
