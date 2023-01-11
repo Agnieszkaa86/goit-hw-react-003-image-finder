d4ac9130b59ebdf5d58bf10af1b223da3c959bb4
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-11 22:56:23 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,31 +3,30 @@ import PropTypes from 'prop-types';
 import { Overlay, ModalWindow } from './Modal.styled';
 
 export default class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener("keydown", this.handleKeyDown);
-    }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener("keydown", this.handleKeyDown);
-    }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-    handleKeyDown = (e) => {
-        if (e.code === "Escape") {
-            return this.props.closeModalWindow();
-        }
-    };
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      return this.props.closeModalWindow();
+    }
+  };
   render() {
     const { item } = this.props;
-       return (
-    <Overlay onClick={this.handleKeyDown}>
-      <ModalWindow>
-        <img src={item} alt="" />
-      </ModalWindow>
-    </Overlay>
-  );
-     }
-  
-};
+    return (
+      <Overlay onClick={this.handleKeyDown}>
+        <ModalWindow>
+          <img src={item} alt="" />
+        </ModalWindow>
+      </Overlay>
+    );
+  }
+}
 
 Modal.propTypes = {
   item: PropTypes.string,
